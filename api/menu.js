006a3d20c0942623486e4b5de03b98527d085e78
Luis Tupa <luis.tupa@neoauto.pe>
2022-12-12 16:24:23 -0500

    feat: update menuPaths list

--- api/menu.js
+++ api/menu.js
@@ -1,17 +1,32 @@
 export const menuPaths = [
   {
-    title: 'Profile',
-    pathName: '/manage/profile',
+    title: 'Mi perfil',
+    pathName: '/anunciante/miperfil/datos-personales',
     icon: 'user'
   },
   {
-    title: 'My ads',
-    pathName: '/manage/active-notices',
+    title: 'Mis avisos',
+    pathName: '/gestion/avisos',
     icon: 'car'
   },
   {
-    title: 'Logout',
-    pathName: '/manage/logout',
+    title: 'Mis interesados',
+    pathName: '/anunciante/micuenta/mis-interesados',
+    icon: 'users'
+  },
+  {
+    title: 'Mis alertas',
+    pathName: '/alerta/mialerta/registro',
+    icon: 'alert'
+  },
+  {
+    title: 'Mis favoritos',
+    pathName: '/anunciante/micuenta/mis-favoritos',
+    icon: 'heart'
+  },
+  {
+    title: 'Cerrar Sesión',
+    pathName: '/cuenta/logout',
     icon: 'logout'
   }
 ]
